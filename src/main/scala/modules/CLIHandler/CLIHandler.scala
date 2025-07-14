c75ac36eff6d4c7ab664ceebb7af5e232755c695
GreyAtlas <velykisgytis@gmail.com>
2025-07-15 02:21:08 +0300

    removed unused file

--- src/main/scala/modules/CLIHandler/CLIHandler.scala
+++
@@ -1,9 +0,0 @@
-// package modules.CLIHandler
-// import modules.IO.IOHandler
-
-// object CLIHandler {
-//   def handleVarargs(
-//       args: String*
-//   ): Some[(locationsPath: String, regionsPath: String, outputPath: String)]=
-  
-// }
