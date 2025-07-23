2295e091acb536cfa161f23ac5cc8e3c37cae0b7
GreyAtlas <velykisgytis@gmail.com>
2025-07-23 18:11:04 +0300

    Added unit tests for the custom Circe Decoders

--- src/main/scala/types/Point.scala
+++ src/main/scala/types/Point.scala
@@ -1,3 +1,24 @@
 package types
 
 final case class Point(longitude: Longitude, latitude: Latitude)
+
+object Point:
+  def apply(
+      longitude: Option[Longitude],
+      latitude: Option[Latitude]
+  ): Option[Point] =
+    (longitude, latitude) match {
+      case (Some(longitude), Some(latitude)) =>
+        Some(new Point(longitude, latitude))
+      case (_, _) => None
+    }
+
+  def apply(
+      longitude: Float,
+      latitude: Float
+  ): Option[Point] =
+    (Longitude(longitude), Latitude(latitude)) match {
+      case (Some(longitude), Some(latitude)) =>
+        Some(new Point(longitude, latitude))
+      case (_, _) => None
+    }
--- src/test/scala/modules/json/CirceJsonParserSuite.scala
+++ src/test/scala/modules/json/CirceJsonParserSuite.scala
@@ -1,31 +1,205 @@
 package modules.json
 
 import io.circe.*
-import io.circe.generic.auto.*
-import io.circe.syntax.*
 import modules.json.CirceJsonParser
 import types.Latitude
 import types.LocationMatchResult
 import types.Longitude
 import types.Point
+import types.Polygon
 
 class CirceJsonParserSuite extends munit.FunSuite {
   test(
-    "When decoding point and coordinates are within bounds"
+    "When decoding longitude and decoded coordinate is within bounds"
   ) {
-    val expected = Point(Longitude(0).get, Latitude(0).get)
+    val expected = Longitude(0.41561616)
+    val jsonString = """0.41561616"""
 
-    val jsonString = expected.asJson.noSpaces
-    val decodedPoint = parser.decode[Point](jsonString)
+    val decodedLongitude =
+      parser.decode[Longitude](jsonString)(using
+        CirceJsonParser.longitudeDecoder
+      )
 
-    decodedPoint match {
-      case Left(value) =>
-        fail(s"Decoding failed: ${value}")
-      case Right(value) => assertEquals(value, expected)
+    (expected, decodedLongitude) match {
+      case (_, Left(error)) =>
+        fail(s"Decoding failed: ${error}")
+      case (None, _) => fail("The expected point is incorrectly formated")
+      case (Some(expected), Right(obtained)) =>
+        assertEquals(obtained, expected)
     }
   }
   test(
-    "When encoding a list of LocationMatchResult and the list is empty returns "
+    "When decoding longitude and decoded coordinate is outside of bounds"
+  ) {
+    val jsonString = """-360.41561616"""
+
+    val decodedLongitude =
+      parser.decode[Longitude](jsonString)(using
+        CirceJsonParser.longitudeDecoder
+      )
+
+    decodedLongitude match {
+      case Left(error: DecodingFailure) =>
+        ()
+      case Left(parsingError: ParsingFailure) =>
+        fail("Json should be parsed correctly", clues(decodedLongitude))
+      case Right(obtained) =>
+        fail(s" Should not return a right ", clues(decodedLongitude))
+    }
+  }
+  test(
+    "When decoding latitude and decoded coordinate is within bounds"
+  ) {
+    val expected = Latitude(0.41561616)
+    val jsonString = """0.41561616"""
+
+    val decodedLatitude =
+      parser.decode[Latitude](jsonString)(using
+        CirceJsonParser.latitudeDecoder
+      )
+
+    (expected, decodedLatitude) match {
+      case (_, Left(error: DecodingFailure)) =>
+        fail(s"Failed when decoding json: ${error}")
+      case (_, Left(error: ParsingFailure)) =>
+        fail(s"Failed when parsing json ${error}")
+      case (None, _) =>
+        fail("The expected point is incorrectly formated", clues(expected))
+      case (Some(expected), Right(obtained)) =>
+        assertEquals(obtained, expected)
+    }
+  }
+  test(
+    "When decoding latitude and decoded coordinate is outside of bounds"
+  ) {
+    val jsonString = """-360.41561616"""
+
+    val decodedLatitude =
+      parser.decode[Latitude](jsonString)(using
+        CirceJsonParser.latitudeDecoder
+      )
+
+    decodedLatitude match {
+      case Left(error: DecodingFailure) =>
+        ()
+      case Left(parsingError: ParsingFailure) =>
+        fail("Json should be parsed correctly", clues(decodedLatitude))
+      case Right(obtained) =>
+        fail(s" Should not return a right ", clues(decodedLatitude))
+    }
+  }
+
+  test(
+    "When decoding point and coordinates are within bounds"
+  ) {
+    val expected = Point(0, 0)
+
+    val jsonString = """[0.0,0.0]"""
+    val decodedPoint =
+      parser.decode[Point](jsonString)(using CirceJsonParser.pointDecoder)
+
+    (expected, decodedPoint) match {
+      case (_, Left(error: DecodingFailure)) =>
+        fail(s"Failed when decoding json: ${error}")
+      case (_, Left(error: ParsingFailure)) =>
+        fail(s"Failed when parsing json ${error}")
+      case (None, _) =>
+        fail("The expected point is incorrectly formated", clues(expected))
+      case (Some(expected), Right(obtained)) => assertEquals(obtained, expected)
+
+    }
+  }
+  val pointDecodingFailureCases = List(
+    ("Longitude out of bounds", "[-360,0]"),
+    ("Longitude out of bounds", "[360,0]"),
+    ("Latitude out of bounds", "[0,-180]"),
+    ("Latitude out of bounds", "[0,180]"),
+    ("Both coordinates are out of bounds", "[-360,-180]"),
+    ("Both coordinates are out of bounds", "[360,180]")
+  )
+  pointDecodingFailureCases.foreach { case (name, jsonString) =>
+    test(
+      s"Out of bounds point decoding test: $name"
+    ) {
+      val decodedPoint =
+        parser.decode[Point](jsonString)(using CirceJsonParser.pointDecoder)
+
+      decodedPoint match {
+        case Left(error: DecodingFailure) =>
+          ()
+        case Left(parsingError: ParsingFailure) =>
+          fail("Json should be parsed correctly", clues(decodedPoint))
+        case Right(obtained) =>
+          fail("Decoding shouldn't return a right", clues(decodedPoint))
+
+      }
+    }
+  }
+  val polygonDecodingFailureCases = List(
+    (
+      "List contains less than 3 distinct vertices",
+      """    
+      [
+        [
+          23.728463251292055,
+          54.85806510526285
+        ],
+        [
+          23.834518591254124,
+          54.815780434232124
+        ],
+        [
+          23.728463251292055,
+          54.85806510526285
+        ]
+      ]
+    """
+    ),
+    (
+      "Last vertex doesn't equal the first",
+      """    
+      [
+        [
+          23.728463251292055,
+          54.85806510526285
+        ],
+        [
+          23.834518591254124,
+          54.815780434232124
+        ],
+        [
+          24.02623401349132,
+          54.815780434232124
+        ],
+        [
+          -23.728463251292055,
+          -54.85806510526285
+        ]
+      ]
+    """
+    )
+  )
+  polygonDecodingFailureCases.foreach { case (name, jsonString) =>
+    test(
+      s"Out of bounds polygon decoding test: $name"
+    ) {
+      val decodedPolygon =
+        parser.decode[Polygon](jsonString)(using CirceJsonParser.polygonDecoder)
+
+      decodedPolygon match {
+        case Left(error: DecodingFailure) =>
+          ()
+        case Left(parsingError: ParsingFailure) =>
+          fail("Json should be parsed correctly", clues(decodedPolygon))
+        case Right(obtained) =>
+          fail("Decoding shouldn't return a right", clues(decodedPolygon))
+
+      }
+    }
+  }
+
+  test(
+    "When encoding a list of LocationMatchResult and the list is empty"
   ) {
     val locationMatchResults: List[LocationMatchResult] = List.empty
 
