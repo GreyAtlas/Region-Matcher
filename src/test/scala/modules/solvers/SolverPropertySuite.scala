6fcd48232efd654074a8c4860bbad892d966a8d2
GreyAtlas <velykisgytis@gmail.com>
2025-07-28 01:53:20 +0300

    updated property suite  to cover more cases

--- src/test/scala/modules/solvers/SolverPropertySuite.scala
+++ src/test/scala/modules/solvers/SolverPropertySuite.scala
@@ -14,6 +14,7 @@ trait SolverPropertySuite extends ScalaCheckSuite {
 
   def solverInstance: Solver
 
+  val coordinateTolerance = 1e-5f
   val rectangleAcrossAntimeridian = Region(
     "region1",
     Vector[Polygon](
@@ -24,6 +25,34 @@ trait SolverPropertySuite extends ScalaCheckSuite {
           Point(Longitude(-170).get, Latitude(-10).get),
           Point(Longitude(-170).get, Latitude(10).get),
           Point(Longitude(170).get, Latitude(10).get)
+        )
+      ).get
+    )
+  )
+  val rectangleAcrossPrimeMeridian = Region(
+    "region1",
+    Vector[Polygon](
+      Polygon(
+        Vector[Point](
+          Point(Longitude(10).get, Latitude(10).get),
+          Point(Longitude(10).get, Latitude(-10).get),
+          Point(Longitude(-10).get, Latitude(-10).get),
+          Point(Longitude(-10).get, Latitude(10).get),
+          Point(Longitude(10).get, Latitude(10).get)
+        )
+      ).get
+    )
+  )
+  val longNorthernRectangle = Region(
+    "region1",
+    Vector[Polygon](
+      Polygon(
+        Vector[Point](
+          Point(Longitude(-180).get, Latitude(70).get),
+          Point(Longitude(-180).get, Latitude(10).get),
+          Point(Longitude(0).get, Latitude(10).get),
+          Point(Longitude(0).get, Latitude(70).get),
+          Point(Longitude(-180).get, Latitude(70).get)
         )
       ).get
     )
@@ -39,9 +68,10 @@ trait SolverPropertySuite extends ScalaCheckSuite {
       Point(Longitude(longitude), Latitude(latitude))
     )
 
-  def pointRectGen(
+  def testParameterGenerator(
       longitudeBound: (Float, Float),
-      latitudeBound: (Float, Float)
+      latitudeBound: (Float, Float),
+      region: Region
   ): Gen[(Location, Region, List[LocationMatchResult])] =
     pointGenerator(
       (longitudeBound._1, longitudeBound._2),
@@ -51,7 +81,7 @@ trait SolverPropertySuite extends ScalaCheckSuite {
         val location = Location("location", point)
         (
           location,
-          rectangleAcrossAntimeridian,
+          region,
           List(
             LocationMatchResult(
               rectangleAcrossAntimeridian.name,
@@ -63,26 +93,139 @@ trait SolverPropertySuite extends ScalaCheckSuite {
       case None => Gen.fail
     }
 
-  val pointInsideRectGen: Gen[(Location, Region, List[LocationMatchResult])] =
-    pointRectGen((170f + 0.01f, 180f), (-10f + 0.01f, 10f - 0.01f))
-
-  val pointOutsideRectGen: Gen[(Location, Region, List[LocationMatchResult])] =
-    pointRectGen((-180f, 170f - 0.01f), (-10f + 0.01f, 10f - 0.01f))
-
-  property(
-    "Location inside rectangle region should return location name in match result"
-  ) {
-    forAll(pointInsideRectGen) { case (location, region, locationMatchResult) =>
-      val result = solverInstance.matchRegionsToLocations(
-        List[Region](region),
-        List[Location](location)
+  val pointCloseToOutsideEquatorEdgeOfLongRectangle
+      : Gen[(Location, Region, List[LocationMatchResult])] =
+    testParameterGenerator(
+      (-180f, 0f),
+      (10f - coordinateTolerance, 10f - coordinateTolerance),
+      longNorthernRectangle
+    )
+  val pointCloseToOutsidePolarEdgeOfLongRectangle
+      : Gen[(Location, Region, List[LocationMatchResult])] =
+    testParameterGenerator(
+      (-180f, 0f),
+      (70f + coordinateTolerance, 70f + coordinateTolerance),
+      longNorthernRectangle
+    )
+  val pointOutsidePrimeMeridianGen
+      : Gen[(Location, Region, List[LocationMatchResult])] =
+    Gen.oneOf(
+      testParameterGenerator(
+        (-180f, -10f + coordinateTolerance),
+        (-90f, 90f),
+        rectangleAcrossPrimeMeridian
+      ),
+      testParameterGenerator(
+        (10f + coordinateTolerance, 180f),
+        (-90f, 90f),
+        rectangleAcrossPrimeMeridian
       )
-      result match {
-        case Left(error) =>
-          fail("Error occured during solving", clues(error, result))
-        case Right(result) => assertEquals(result, locationMatchResult)
-      }
+    )
+  val pointOutsideAntimeridianRectGen
+      : Gen[(Location, Region, List[LocationMatchResult])] =
+    testParameterGenerator(
+      (-170f + coordinateTolerance, 170f - coordinateTolerance),
+      (-90f, 90f),
+      rectangleAcrossAntimeridian
+    )
+  val generatedResultCannotEqualCases = List(
+    ("Location outside prime meridian rectangle", pointOutsidePrimeMeridianGen),
+    (
+      "Location close the equator facing outside edge of long rectangle",
+      pointCloseToOutsideEquatorEdgeOfLongRectangle
+    ),
+    (
+      "Location close to the pole facing outside edge of long rectangle",
+      pointCloseToOutsidePolarEdgeOfLongRectangle
+    ),
+    (
+      "Location outside antimeridian rectangle",
+      pointOutsideAntimeridianRectGen
+    )
+  )
 
+  generatedResultCannotEqualCases.foreach { case (name, pointGenerator) =>
+    property(
+      s"Generated result should not equal solver result: ${name}"
+    ) {
+      forAll(pointGenerator) { case (location, region, locationMatchResult) =>
+        val result = solverInstance.matchRegionsToLocations(
+          List[Region](region),
+          List[Location](location)
+        )
+        result match {
+          case Left(error) =>
+            fail("Error occured during solving", clues(error, result))
+          case Right(result: List[LocationMatchResult]) =>
+            assertNotEquals(result, locationMatchResult)
+        }
+      }
+    }
+  }
+  val pointInsidePrimeMeridianRectGen
+      : Gen[(Location, Region, List[LocationMatchResult])] =
+    testParameterGenerator(
+      (-10f + coordinateTolerance, 10f - coordinateTolerance),
+      (-10f + coordinateTolerance, 10f - coordinateTolerance),
+      rectangleAcrossPrimeMeridian
+    )
+
+  val pointInsideNegativeAntimeridianRectGen
+      : Gen[(Location, Region, List[LocationMatchResult])] =
+    testParameterGenerator(
+      (-180f, -170f - coordinateTolerance),
+      (-10f + coordinateTolerance, 10f - coordinateTolerance),
+      rectangleAcrossAntimeridian
+    )
+  val pointInsidePositiveAntimeridianRectGen
+      : Gen[(Location, Region, List[LocationMatchResult])] =
+    testParameterGenerator(
+      (170f + coordinateTolerance, 180f),
+      (-10f + coordinateTolerance, 10f - coordinateTolerance),
+      rectangleAcrossAntimeridian
+    )
+  val pointCloseToInsideEquatorEdgeOfLongRectangle
+      : Gen[(Location, Region, List[LocationMatchResult])] =
+    testParameterGenerator(
+      (-180f, 0f),
+      (10f + coordinateTolerance, 10f + coordinateTolerance),
+      longNorthernRectangle
+    )
+  val generatedResultMustEqualCases = List(
+    (
+      "Location inside prime meridian rectangle",
+      pointInsidePrimeMeridianRectGen
+    ),
+    (
+      "Location inside the negative side of an antimeridian rectangle",
+      pointInsideNegativeAntimeridianRectGen
+    ),
+    (
+      "Location inside the positive side of an antimeridian rectangle",
+      pointInsidePositiveAntimeridianRectGen
+    ),
+    (
+      "Location close to the inside equator facing edge of a long rectangle",
+      pointCloseToInsideEquatorEdgeOfLongRectangle
+    )
+  )
+
+  generatedResultMustEqualCases.foreach { case (name, pointGenerator) =>
+    property(
+      s"Generated result must equal solver result: ${name}"
+    ) {
+      forAll(pointGenerator) { case (location, region, locationMatchResult) =>
+        val result = solverInstance.matchRegionsToLocations(
+          List[Region](region),
+          List[Location](location)
+        )
+        result match {
+          case Left(error) =>
+            fail("Error occured during solving", clues(error, result))
+          case Right(result: List[LocationMatchResult]) =>
+            assertEquals(result, locationMatchResult)
+        }
+      }
     }
   }
 }
